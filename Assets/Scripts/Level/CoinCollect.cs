88bd3845a703ac3f31e09b6fa81d38491ddf94a6
Mayonnaise <156632738+Jasonliew99@users.noreply.github.com>
2025-11-14 03:41:16 +0800

    Added some animation stuff & Coins visibility based on torchlight

--- Assets/Scripts/Level/CoinCollect.cs
+++ Assets/Scripts/Level/CoinCollect.cs
@@ -7,9 +7,14 @@ public class CoinCollect : MonoBehaviour
     public int value = 1;
 
     private LevelController levelController;
+    private TorchlightRevealItem revealItem;
 
     private void Start()
     {
+        revealItem = GetComponent<TorchlightRevealItem>();
+        if (revealItem == null)
+            Debug.LogWarning($"[{name}] TorchlightRevealItem missing!");
+
         // Automatically find the nearest active LevelController in the scene
         levelController = FindObjectOfType<LevelController>();
         if (levelController == null)
@@ -22,19 +27,18 @@ public class CoinCollect : MonoBehaviour
     {
         if (!other.CompareTag("Player")) return;
 
+        TorchlightRevealItem revealItem = GetComponent<TorchlightRevealItem>();
+        if (revealItem != null && !revealItem.CanBeCollected())
+            return; // can't collect if hidden
+
         // Add to player's coin total
         PlayerCoinCollector collector = other.GetComponent<PlayerCoinCollector>();
         if (collector != null)
-        {
             collector.AddCoins(value);
-        }
 
         // Notify LevelController
         if (levelController != null)
-        {
-            Debug.Log($"[{name}] notifying {levelController.name} that coin was collected.");
             levelController.CoinCollected();
-        }
 
         // Disable coin
         gameObject.SetActive(false);
